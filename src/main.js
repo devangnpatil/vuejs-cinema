134230258af3c9d6f5b1516013f090e92590c45b
devang <devang.patil@upgrad.com>
2019-01-05 11:49:37 +0530

    add components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,26 @@ import Vue from 'vue';
 
 new Vue({
 	el: '#app',
-	data: {
-		msg: 'Hello Worlds'	
+	components: {
+		'movie-list': {
+			template: `<div id="movie-list">
+						<div v-for="movie in movies" class="movie">{{ movie.title }}</div>	
+					</div>`,
+			data: function(){
+				return {
+					movies: [
+						{ title: 'Pulp fiction 1'},
+						{ title: 'Pulp fiction 2'},
+						{ title: 'Pulp fiction 3'},
+						{ title: 'Pulp fiction 4'},
+					]
+				};
+			}
+		},
+		'movie-filter': {
+			template: `<div id="movie-filter">
+				<h2>Filter results</h2>
+			</div>`	
+		}
 	}
 });
